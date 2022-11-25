9b7258967f9bbfbf08fe607c4ed0974b1856636e
Leonardo Paredes <110631380+lmparedes1977@users.noreply.github.com>
2022-11-24 21:52:21 -0300

    Update Aula 10.py

--- Aula 10.py
+++ Aula 10.py
@@ -20,3 +20,53 @@ print()
 print(f'Na lista coletada, a maior entrada foi {maior} na posição {lista.index(maior)}'
       f' e a menor entrada foi {menor} na posição {lista.index(menor)}')
 
+# [Exercício 2] Escreva um programa que o usuário escreverá vários valores  numéricos e cadastre-os em uma lista.
+# Caso o número já exista lá dentro, ele não será adicionado.
+# No final, serão exibidos todos os valores únicos digitados, e em ordem crescente.
+
+'''lista = []
+temp = 0
+while True:
+    num = int(input('Entre com um valor inteiro: '))
+    if num not in lista:
+        lista.append(num)
+        print('\033[32mValor acrescentado.\033[m')
+    else:
+        print('\033[31mValor já consta na lista.\033[m')
+    continuar = str(input("Deseja continuar? [S/N] ")).upper().strip()[0]
+    while continuar not in "SN":
+        continuar = str(input("INVALIDO. Digitar 'S' ou 'N' ")).upper().strip()[0]
+    if continuar == 'N':
+        break
+
+temp = 0
+for i in range(0, len(lista)-1):
+    for j in range(i+1, len(lista)):
+        if lista[i] > lista[j]:
+            temp = lista[i]
+            lista[i] = lista[j]
+            lista[j] = temp
+
+print(f'\n\033[36mLista = {lista}\033[m')'''
+
+# [Exercício 3] Escreva um programa que o user digite 5 valores numéricos
+# e cadastre-os em uma lista, já na posição correta de inserção (SEM USAR O sort()).
+# No final, mostre a lista ordenada na tela.
+
+print('ENTRE COM 5 NÚMEROS INTEIROS:')
+lista5 = []
+lista5.append(int(input('1º: ')))
+for i in range(1, 5):
+    num = int(input(f'{i+1}º: '))
+    if num < lista5[i]:
+        lista5.insert(i, num)
+    elif num < lista5[i+1]:
+        lista5.insert(i+1, num)
+    elif num < lista5[i+2]:
+        lista5.insert(i+2, num)
+    else:
+        lista5.insert(i+3, num)
+
+
+
+print(lista5)
